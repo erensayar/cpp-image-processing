e6d8902be17bfee679b6b6ff9c4bbaa7fa500610
erensayar <erensayar@yandex.com>
2022-11-23 16:44:13 +0300

    improvement

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-#ifdef _WIN32
-// Windows
-#else
-// Unix
+#ifdef _WIN32 // Windows
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#else // Unix
+#include <iostream>
 #include </usr/local/include/opencv4/opencv2/opencv.hpp>
 #include </usr/local/include/opencv4/opencv2/imgcodecs.hpp>
 #include </usr/local/include/opencv4/opencv2/highgui.hpp>
 #include </usr/local/include/opencv4/opencv2/highgui/highgui.hpp>
 #include </usr/local/include/opencv4/opencv2/imgproc.hpp>
-#include <iostream>
 #endif
 
 using namespace cv;
@@ -16,11 +21,13 @@ using namespace std;
 
 /////////////////  Images  //////////////////////
 int main() {
-    string path = "~/MEGA/CODES/C-Cpp/cpp-image-processing/resources/paper.png";
+    //string path = "~/MEGA/CODES/C-Cpp/cpp-image-processing/resources/paper.jpg";
+    string path = "C:/Users/xresayar/Desktop/Codes/Codes-Personal/C++/cpp-image-processing/resources/paper.jpg";
     Mat img = imread(path);
     Mat gray;
     bilateralFilter(img, gray, 10, 50, 0);
-    imwrite("~/Documents", gray);
+    //imwrite("~/Documents", gray);
+    imwrite("C:/Users/xresayar/Desktop", gray);
     //imshow("Image", img);
     //waitKey(0);
     return 0;
