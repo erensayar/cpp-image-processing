853619fa803b5dde63abd1d1be53f800a0c66d89
erensayar <erensayar@yandex.com>
2022-11-24 10:59:24 +0300

    improvement

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-#ifdef _WIN32 // Windows
+#ifdef _WIN32 // Windows Dependencies
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
 
-#else // Unix
+#else // Unix Dependencies
 #include <iostream>
 #include </usr/local/include/opencv4/opencv2/opencv.hpp>
 #include </usr/local/include/opencv4/opencv2/imgcodecs.hpp>
@@ -18,18 +16,16 @@
 using namespace cv;
 using namespace std;
 
+#if defined(_GLIBCXX_HAS_GTHREADS) && defined(_GLIBCXX_USE_C99_STDINT_TR1)
+#endif
 
 /////////////////  Images  //////////////////////
 int main() {
     //string path = "~/MEGA/CODES/C-Cpp/cpp-image-processing/resources/paper.jpg";
     string path = "C:/Users/xresayar/Desktop/Codes/Codes-Personal/C++/cpp-image-processing/resources/paper.jpg";
     Mat img = imread(path);
-    Mat gray;
-    bilateralFilter(img, gray, 10, 50, 0);
-    //imwrite("~/Documents", gray);
-    imwrite("C:/Users/xresayar/Desktop", gray);
-    //imshow("Image", img);
-    //waitKey(0);
+    imshow("Image", img);
+    waitKey(0);
     return 0;
 }
 
