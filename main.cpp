16b0cd35fcb34379706db4c8160e6163d233a59b
erensayar <erensayar@yandex.com>
2022-11-28 09:21:29 +0300

    add cmake module path

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #ifdef _WIN32 // Windows Dependencies
 #include <iostream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 
 #else // Unix Dependencies
