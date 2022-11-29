2275a60e94adf0e6c0c43bf030a3ff048b7d660c
erensayar <erensayar@yandex.com>
2022-11-29 10:45:56 +0300

    create options as simple menu

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,93 @@
 using namespace cv;
 using namespace std;
 
-// <================== Image =================>
+// METHODS
+// <====================================================================================>
+Mat readImage(string path) {
+    Mat image = imread(path, 1);
+    if (!image.data) {
+        cout << ("No image data");
+        return Mat();
+    }
+    return image;
+}
+
+void showImage(Mat image){
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+}
+
+void videoCapture(string path){
+   VideoCapture cap(path);
+   Mat img;
+   while (true) {
+       cap.read(img);
+       imshow("Image", img);
+       waitKey(20);
+   }
+}
+
+void webcamCapture() {
+    VideoCapture cap(0);
+    Mat img;
+    while (true) {
+        cap.read(img);
+        imshow("Image", img);
+        waitKey(1);
+    }
+}
+
+// MAIN
+// <====================================================================================>
+
+int main(int argc, char **argv) {
+
+    int input;
+    string path;
+    Mat img;
+    bool loopVal = true;
+
+    while (loopVal) {
+        cout << "Choose Process" << endl;
+        cout << "1. Show Image" << endl;
+        cout << "2. Video Capture" << endl;
+        cout << "3. Show Video" << endl;
+
+        cout << "INPUT: ";
+        cin >> input; 
+
+        switch(input) {
+            case 1:
+                cout << "Enter image path according to your path: ";
+                cin >> path;
+                img = readImage(path);
+                showImage(img);
+                break;
+            case 2:
+                cout << "Enter video path according to your path: ";
+                cin >> path;
+                videoCapture(path);
+                break;
+            case 3:
+                webcamCapture();
+                break;
+            default:
+                cout << "Enter valid value\n";
+        }
+    }
+    
+    return 0;
+}
+
+// NOTES
+// <====================================================================================>
+
+// EXAMPLE : Image path take as argument
+/*
 int main(int argc, char **argv) {
     if (argc != 2) {
-        cout << ("USAGE: App <Image_Path>");
+        cout << ("USAGE: app.exe <Image_Or_Video_Path>");
         return -1;
     }
     Mat image = imread(argv[1], 1);
@@ -25,34 +108,5 @@ int main(int argc, char **argv) {
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
     waitKey(0);
-    return 0;
-}
-
-// <================== Video =================>
-/*
-int main() {
-   string path = "../resources/test_video.mp4";
-   VideoCapture cap(path);
-   Mat img;
-   while (true) {
-       cap.read(img);
-       imshow("Image", img);
-       waitKey(20);
-   }
-   return 0;
-}
-*/
-
-// <================= Webcam =================>
-/*
-int main() {
-    VideoCapture cap(0);
-    Mat img;
-    while (true) {
-        cap.read(img);
-        imshow("Image", img);
-        waitKey(1);
-    }
-    return 0;
 }
 */
